f66dadc75495337ad0f76957fd63fc7639d3d5c9
kimminchae <0720kmc@naver.com>
2024-10-24 17:46:49 +0900

    20241024 김민채 메인페이지 작성중

---
+++ HOPE_DOG/src/main/java/com/example/hope_dog/mapper/adopt/adopt/AdoptMapper.java
@@ -0,0 +1,11 @@
+package com.example.hope_dog.mapper.adopt.adopt;
+
+import com.example.hope_dog.dto.adopt.adopt.AdoptMainDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AdoptMapper {
+    List<AdoptMainDTO> adoptMain();
+}
---
+++ HOPE_DOG/src/main/java/com/example/hope_dog/service/adopt/adopt/AdoptService.java
@@ -0,0 +1,18 @@
+package com.example.hope_dog.service.adopt.adopt;
+
+import com.example.hope_dog.dto.adopt.adopt.AdoptMainDTO;
+import com.example.hope_dog.mapper.adopt.adopt.AdoptMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdoptService {
+    private AdoptMapper adoptMapper;
+
+//    public List<AdoptMainDTO> getAdoptList() {
+//        return adoptMapper.adoptMain();
+//    }
+
+}
