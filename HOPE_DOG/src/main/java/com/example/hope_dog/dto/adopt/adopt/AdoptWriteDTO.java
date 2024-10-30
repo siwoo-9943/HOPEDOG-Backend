90c69ecb5b1f81c15a1bc641db1bcc1cd95e007b
kimminchae <0720kmc@naver.com>
2024-10-30 19:19:18 +0900

    20241030 김민채 게시글작성/댓글불러오기/페이지네이션 완료

--- HOPE_DOG/src/main/java/com/example/hope_dog/dto/adopt/adopt/AdoptWriteDTO.java
+++ HOPE_DOG/src/main/java/com/example/hope_dog/dto/adopt/adopt/AdoptWriteDTO.java
@@ -1,18 +1,24 @@
 package com.example.hope_dog.dto.adopt.adopt;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
 import java.util.Date;
 
+@Getter @Setter
 public class AdoptWriteDTO {
     private Long adoptNo;
-    private Date adoptPeriodStart;
-    private Date adoptPeriodEnd;
+    private LocalDate adoptPeriodstart;
+    private LocalDate adoptPeriodend;
     private String adoptName;
     private String adoptBreed;
     private String adoptNeutering;
     private String adoptGender;
     private String adoptWeight;
-    private String adoptBirth;
+    private LocalDate adoptBirth;
     private String adoptIntroduce;
     private String adoptTitle;
     private String adoptContent;
+    private String centerMemberNo;
 }
